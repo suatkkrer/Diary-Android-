9e3cc4de596dc8bbe9845b95923abea1d8301324
suatkkrer <suat.kukrer@gmail.com>
2020-11-03 10:50:27 +0100

    password bug fixed

--- app/src/main/java/com/suatkkrer/diary/PasswordNewFirst.java
+++ app/src/main/java/com/suatkkrer/diary/PasswordNewFirst.java
@@ -139,7 +139,8 @@ public class PasswordNewFirst extends AppCompatActivity {
 
                 sqLiteDatabase.execSQL("INSERT INTO password (pass) VALUES ('" + passLast + "')");
 
-                Intent intent = new Intent(getApplicationContext(),PasswordActivity.class);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Toast.makeText(this, "Şifreniz Başarıyla Oluşturuldu", Toast.LENGTH_LONG).show();
                 startActivity(intent);
 
             } catch (Exception e){
